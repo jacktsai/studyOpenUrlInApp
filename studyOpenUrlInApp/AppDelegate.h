c57aba01d396353c07372bd318d83da14f640d07
Jack Tsai <wa0327@gmail.com>
2017-05-10 18:20:06 +0800

    Initial Commit

---
+++ studyOpenUrlInApp/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  studyOpenUrlInApp
+//
+//  Created by Jack Tsai on 2017/5/10.
+//  Copyright © 2017年 wa0327. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
